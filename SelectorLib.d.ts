bd1064b5e44933c600300cc7b4c1fdaadc8670f7
alabsi91 <58077449+alabsi91@users.noreply.github.com>
2020-08-30 15:56:45 +0300

    s

--- SelectorLib.d.ts
+++ SelectorLib.d.ts
@@ -2,7 +2,7 @@ import * as CSS from "./index"
 
 export declare class domElement {
     constructor(targets: String | Element);
-};
+}
 
 export interface domElement {
     /**
@@ -40,7 +40,7 @@ export interface domElement {
      * 
      * **Syntax**: `S(targets).clone(deep)`
     */
-    clone(element: Boolean = false): void,
+    clone(element: Boolean): void,
     /**
      * Set Attribute for an HTML element
      * 
@@ -97,7 +97,7 @@ export interface domElement {
      * @param {Boolean} [toNumber] - determine the return value type `(string|number)`
      * 
     */
-    getStyle(property: keyof CSS.StandardPropertiesHyphen, toNumber = false): String | Number,
+    getStyle(property: keyof CSS.StandardPropertiesHyphen, toNumber?: Boolean): String | Number,
     /**
      * Get the style variable from DOM Root
      * 
@@ -106,7 +106,7 @@ export interface domElement {
      * @param {Boolean} [toNumber] - determine the return value type `(string|number)`
      * 
     */
-    getCssVar(variable: String, toNumber = false): String | Number,
+    getCssVar(variable: String, toNumber?: Boolean): String | Number,
     /**
      * Set a Css style variable
      * 
@@ -122,7 +122,7 @@ export interface domElement {
      * @param {Number} time - time in milliseconds
      * 
     */
-    time(time: Number): Promise,
+    time(time: Number): void,
     /**
      * Animate a dom element through CSS properties
      *
@@ -132,7 +132,7 @@ export interface domElement {
     scrollTo(options: ScrollToOptions): void,
 }
 
-export declare const S: (targets: any) => domElement
+export declare const S: (targets: String | Element) => domElement
 
 type attributes = "autocomplete" | "async" | "alt" | "class" | "contenteditable" | "dir" | "draggable" | "hidden" | "id" | "lang" | "style" | "src" | "title" | "translate" | "value";
 
@@ -162,7 +162,7 @@ interface AnimateOptions {
      *
      * **Initial value**: `false`
     */
-    cleanUp?= false,
+    cleanUp?: Boolean,
     /**
      * Remove CSS Property during the animation.
      *
@@ -180,7 +180,7 @@ interface AnimateOptions {
      *
      * **Initial value**: `false`
     */
-    dispalyNone?= false,
+    dispalyNone?: Boolean,
     /**
      * Repeat the animation number of times. infinity if equals to -1
      *
@@ -188,7 +188,7 @@ interface AnimateOptions {
      *
      * **Initial value**: `0`
     */
-    dispalyNone?= 0,
+    repeat?: Number,
     /**
      * animate the element back to the first state.
      *
@@ -196,7 +196,7 @@ interface AnimateOptions {
      *
      * **Initial value**: `false`
     */
-    yoyo?= false,
+    yoyo?: Boolean,
     /**
      * Delay the yoyo animation in milliseconds
      *
@@ -228,7 +228,7 @@ interface AnimateOptions {
      *
      * **Initial value**: `0`
     */
-    delayOnce?= false,
+    delayOnce?: Boolean,
     /**
      * Animation duration in milliseconds : Number
      *
@@ -236,7 +236,7 @@ interface AnimateOptions {
      *
      * **Initial value**: `300`
     */
-    duration?= 300,
+    duration?: Number,
 }
 
 interface ScrollToOptions {
@@ -279,7 +279,7 @@ interface ScrollToOptions {
      * 
      * **Initial value**: `30`
     */
-    pps?= 30,
+    pps?: Number,
     /**
      * Offset vertically on the y-axis from the destination.
      *
@@ -287,7 +287,7 @@ interface ScrollToOptions {
      * 
      * **Initial value**: `0`
     */
-    offsetY?= 0,
+    offsetY?: Number,
     /**
      * Offset vertically on the x-axis from the destination.
      *
@@ -295,7 +295,7 @@ interface ScrollToOptions {
      * 
      * **Initial value**: `0`
     */
-    offsetX?= 0,
+    offsetX?: Number,
 }
 type GlobalEventHandlersEventMap =
     "abort" |
